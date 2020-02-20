ba92a771573413f04f311ab428bbb41acc94ba4a
qasembanyissa <q0790405801@gmail.com>
2020-02-20 12:28:31 +0200

    sum

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function sum(a, b) { //eslint-disable-line
     var message = `The sum of ${a} and ${b} is ${theSum}.`;
     return [theSum, message];
 }
-testSum(4,7);
+testSum();
 
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
